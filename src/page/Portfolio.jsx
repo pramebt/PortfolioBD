fc094feffeb27ec71e02ad4e9316dd0a89571a65
prame BT <pramebt@gmail.com>
2024-11-10 14:52:17 +0700

    a

--- src/page/Portfolio.jsx
+++ src/page/Portfolio.jsx
@@ -1,13 +1,104 @@
-import React from 'react'
+import React from "react";
 
 const Portfolio = () => {
   return (
-    <div className="bg-[#FAFAFA] h-screen w-screen scroll-py-10">
-      <div className="absolute  top-[5%] left-[50%] translate-x-[-50%] space-x-3 text-[48px]">
-        <label className="text-[#005691]  font-extrabold">Portfolio</label>
+    <div className="bg-[#FAFAFA] h-screen w-screen scroll-py-10 overflow-x-hidden">
+      <div className="absolute border-2 border-red-345  top-[5%] left-[50%] translate-x-[-50%]  text-[48px]">
+        <div className="text-center space-x-3">
+        <label className="text-[#000000] font-extrabold">About</label>
+        <label className="text-[#005691]  font-extrabold">Me</label>
+        </div>
+        <div className="border-2 border-red-400 grid lg:grid-cols-3 md:grid-cols-2 gap-5 mt-[80px] w-fit mx-auto">
+          <div className="bg-[#191919] h-auto w-auto rounded-[24px] overflow-hidden">
+            <img
+              src="https://www.viewhotels.jp/asakusa-annex/wp-content/uploads/sites/6/2020/03/test-img.jpg"
+              alt=""
+            />
+            <div className="p-5 space-y-2">
+              <h2 className="text-white text-[24px]">
+                <a href="#">View All Projects</a>
+              </h2>
+              <p className="text-[15px] text-white text-start">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Veritatis, eligendi.
+              </p>
+            </div>
+          </div>
+          <div className="bg-[#191919] h-auto w-auto rounded-[24px] overflow-hidden">
+            <img
+              src="https://www.viewhotels.jp/asakusa-annex/wp-content/uploads/sites/6/2020/03/test-img.jpg"
+              alt=""
+            />
+            <div className="p-5 space-y-2">
+              <h2 className="text-white text-[24px]">
+                <a href="#">View All Projects</a>
+              </h2>
+              <p className="text-[15px] text-white text-start">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Veritatis, eligendi.
+              </p>
+            </div>
+          </div><div className="bg-[#191919] h-auto w-auto rounded-[24px] overflow-hidden">
+            <img
+              src="https://www.viewhotels.jp/asakusa-annex/wp-content/uploads/sites/6/2020/03/test-img.jpg"
+              alt=""
+            />
+            <div className="p-5 space-y-2">
+              <h2 className="text-white text-[24px]">
+                <a href="#">View All Projects</a>
+              </h2>
+              <p className="text-[15px] text-white text-start">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Veritatis, eligendi.
+              </p>
+            </div>
+          </div><div className="bg-[#191919] h-auto w-auto rounded-[24px] overflow-hidden">
+            <img
+              src="https://www.viewhotels.jp/asakusa-annex/wp-content/uploads/sites/6/2020/03/test-img.jpg"
+              alt=""
+            />
+            <div className="p-5 space-y-2">
+              <h2 className="text-white text-[24px]">
+                <a href="#">View All Projects</a>
+              </h2>
+              <p className="text-[15px] text-white text-start">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Veritatis, eligendi.
+              </p>
+            </div>
+          </div><div className="bg-[#191919] h-auto w-auto rounded-[24px] overflow-hidden">
+            <img
+              src="https://www.viewhotels.jp/asakusa-annex/wp-content/uploads/sites/6/2020/03/test-img.jpg"
+              alt=""
+            />
+            <div className="p-5 space-y-2">
+              <h2 className="text-white text-[24px]">
+                <a href="#">View All Projects</a>
+              </h2>
+              <p className="text-[15px] text-white text-start">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Veritatis, eligendi.
+              </p>
+            </div>
+          </div><div className="bg-[#191919] h-auto w-auto rounded-[24px] overflow-hidden">
+            <img
+              src="https://www.viewhotels.jp/asakusa-annex/wp-content/uploads/sites/6/2020/03/test-img.jpg"
+              alt=""
+            />
+            <div className="p-5 space-y-2">
+              <h2 className="text-white text-[24px]">
+                <a href="#">View All Projects</a>
+              </h2>
+              <p className="text-[15px] text-white text-start">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Veritatis, eligendi.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
